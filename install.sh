55aaa95163f4f2b89171c9fd2321785d132ec892
Hazel Rella <hazelrella11@gmail.com>
2025-04-09 04:06:23 -0700

    fix: install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 main() {
     pacman -Sy --noconfirm git uv
     git clone https://github.com/HazelTheWitch/demeter.git
-    uv --directory demeter run main.py
+    exec uv --directory demeter run main.py
 }
 
 main
